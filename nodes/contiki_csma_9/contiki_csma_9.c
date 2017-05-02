e78b244787129c166cbf9ba709a28044bd34cd2b
lakshbhatia <bhatialaksh3@gmail.com>
2017-05-02 17:47:56 +0100

    Multiple clusterheads with flash support

--- nodes/contiki_csma_9/contiki_csma_9.c
+++ nodes/contiki_csma_9/contiki_csma_9.c
@@ -54,24 +54,25 @@
 #define REQSEQ 3
 #define REPSEQ 4
 #define BACKOFF 5
-#define DRONE0 0x9F
-#define DRONE1 0x58
-#define COORD00 0xEC
-#define COORD01 0xB6
-#define COORD10 0x03
-#define COORD11 0x00
+// #define DRONE0 0x9F
+// #define DRONE1 0x58
+#define DRONE0 0x0A
+#define DRONE1 0x00
+
+struct coord{
+    uint8_t first;
+    uint8_t second;
+};
+// struct coord coords[] = {{0xEC,0xB6},{0x9E,0xC8}};
+struct coord coords[] = {{0x03,0x00},{0x05,0x00}};
+
+#define NUMRECORDS sizeof(coords) / sizeof(struct coord)
 
 #if WRITE_TO_FLASH
 #define FILENAME "packets"
 #include "cfs/cfs.h"
 #include "cfs/cfs-coffee.h"
 #endif
-// #define DRONE0 0x0A
-// #define DRONE1 0x00
-// #define COORD00 0x05
-// #define COORD01 0x00
-// #define COORD10 0x03
-// #define COORD11 0x00
 
 /*---------------------------------------------------------------------------*/
 PROCESS(main_process, "Main process");
@@ -91,14 +92,16 @@ volatile int sending = 0;
 volatile int recieving = 0;
 volatile int i = 0;
 volatile int num_packets = NUMPACKETS;
-volatile uint16_t packets_received = 0;
-volatile uint16_t packets_sent = 0;
+// volatile uint16_t packets_received = 0;
+// volatile uint16_t packets_sent = 0;
 static struct ctimer timer;
 volatile int read_since_reset = 0;
-struct record {
+volatile struct record {
+    linkaddr_t from;
+    linkaddr_t to;
     uint16_t recv;
     uint16_t sent;
-}record;
+}records[NUMRECORDS];
 struct packet{
     uint16_t req_reply;
     uint16_t sequenceno;
@@ -150,46 +153,29 @@ PROCESS_THREAD(button_press,ev,data){
 
   while(1){
     PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
+    
     #if WRITE_TO_FLASH
-    if(read_since_reset == 0){
-      fd = cfs_open(FILENAME, CFS_READ);
-      if(fd < 0) {
-        printf("failed to open %s\n", FILENAME);
-      }else{
-        printf("Reading first time %d\n",fd);  
-        r = cfs_read(fd, &record, sizeof(record));
-        if(r == 0) {
-          printf("New one\n");
-        } else if(r < sizeof(record)) { /* 0x00 Error */ 
-          printf("failed to read %d bytes from %s, recv %d sent %d\n",
-             (int)sizeof(record), FILENAME, record.recv,record.sent);
-          packets_received = packets_received + record.recv;
-          packets_sent = packets_sent + record.sent;
-        }else{
-          packets_received = packets_received + record.recv;
-          packets_sent = packets_sent + record.sent;
-          printf("Read %d %d\n",packets_received,packets_sent);
+        fd = cfs_open(FILENAME, CFS_READ | CFS_WRITE);
+        if(fd < 0) {
+          printf("failed to open %s\n", FILENAME);
+        }
+        r = cfs_write(fd, &records, sizeof(records));
+        if(r != sizeof(records)) {
+          printf("failed to write %d bytes to %s,written %d\n",
+           (int)sizeof(records), FILENAME,r);
         }
         cfs_close(fd);
-      }
-      read_since_reset = 1;
-    }
-
-    record.recv = packets_received;
-    record.sent = packets_sent;
-
-    fd = cfs_open(FILENAME, CFS_READ | CFS_WRITE);
-    if(fd < 0) {
-      printf("failed to open %s\n", FILENAME);
-    }
-    r = cfs_write(fd, &record, sizeof(record));
-    if(r != sizeof(record)) {
-      printf("failed to write %d bytes to %s,written %d\n",
-       (int)sizeof(record), FILENAME,r);
-    }
-    cfs_close(fd);
     #endif
-    printf("Received Packets: %d, Sent Packets: %d\n", packets_received,packets_sent);
+    // printf("Received Packets: %d, Sent Packets: %d\n", packets_received,packets_sent);
+    if(is_drone){
+        int j;
+        for(j = 0;j < NUMRECORDS;j++){
+            printf("From: %x.%x Received %d\n", (records[j].from).u8[0],(records[j].from).u8[1],records[j].recv);
+        }
+    }
+    if(is_coordinator){
+        printf("To: %x.%x Sent %d\n", (records[0].to).u8[0],(records[0].to).u8[1],records[0].sent);
+    }
   }
   PROCESS_END();
 }
@@ -288,7 +274,13 @@ recv_drone_uc_1(struct unicast_conn *c, const linkaddr_t *from){
     }  
     if(is_drone){
         printf("Unicast Received Seq No: %d\n",payload.sequenceno);
-        packets_received++;
+        // packets_received++;
+        int j;
+        for(j = 0;j < NUMRECORDS;j++){
+            if((records[j].from.u8[0] == current_recv.u8[0]) && (records[j].from.u8[1] == current_recv.u8[1])){
+                records[j].recv = records[j].recv + 1;
+            }
+        }
         received_seq = received_seq ^ (1 << payload.sequenceno); /* Account for wrong duplicates */
         if(received_seq == 0){
             /* Send backoff */
@@ -309,14 +301,19 @@ recv_drone_uc(struct broadcast_conn *c, const linkaddr_t *from)//, uint8_t seqno
 {
   int d = packetbuf_copyto(&payload);
  
-  printf("Broadcast message received from %d.%d length %d Seq:%d Tot:%d Message type:%dDrone\n",
+  printf("Broadcast message received from %x.%x length %d Seq:%d Tot:%d Message type:%dDrone\n",
      from->u8[0], from->u8[1],d,payload.sequenceno,payload.totalpackets,payload.req_reply);
   leds_toggle(LEDS_GREEN);
   if(is_drone){// && from->u8[0]==coordinator0 && from->u8[1]==coordinator1){
-    packets_received++;
-
+    // packets_received++;
     current_recv.u8[0] = from->u8[0];
     current_recv.u8[1] = from->u8[1];
+    int j;
+    for(j = 0;j < NUMRECORDS;j++){
+        if(((records[j].from).u8[0] == current_recv.u8[0]) && ((records[j].from).u8[1] == current_recv.u8[1])){
+            records[j].recv = records[j].recv + 1;
+        }
+    }
     if(payload.req_reply == REPLY){
       if(received_seq == 0){
         ctimer_restart(&timer);
@@ -338,11 +335,14 @@ sent_drone_uc(struct broadcast_conn *c,  int status, int num_tx)
 // sent_drone_uc(struct unicast_conn *c, int status, int num_tx)
 // sent_drone_uc(struct runicast_conn *c, const linkaddr_t *to, uint8_t num_tx)
 {
-  printf("Broadcast messasge sent status:%d transmission_num %d Drone\n",
+  printf("Broadcast message sent status:%d transmission_num %d Drone\n",
     status,num_tx);
   leds_toggle(LEDS_YELLOW);
-  packets_sent++;
+  // packets_sent++;
   sending = 0;
+  if(is_coordinator){
+    records[0].sent = records[0].sent + 1;
+  }
 }
 /*---------------------------------------------------------------------------*/
 static const struct unicast_callbacks sensor_callbacks = {recv_uc, sent_uc};
@@ -362,17 +362,38 @@ PROCESS_THREAD(main_process, ev, data)
     cfs_coffee_format();
   #endif
 
+  #if WRITE_TO_FLASH
+    int fd;
+    int r;
+    fd = cfs_open(FILENAME, CFS_READ);
+    if(fd < 0) {
+        printf("failed to open %s\n", FILENAME);
+    }else{
+        printf("Reading first time %d\n",fd);  
+        r = cfs_read(fd, &records, sizeof(records));
+        if(r == 0) {
+        printf("New one\n");
+        }else if(r < sizeof(records)) { /* 0x00 Error */ 
+        printf("failed to read %d bytes from %s\n",
+             (int)sizeof(records), FILENAME);
+        }else{
+            printf("Read Properly\n");
+        }
+    cfs_close(fd);
+    }
+  #endif
   // powertrace_start(CLOCK_SECOND * 2);
 
   printf("Hello, Laksh\n");
 
   /* Receiver node: do nothing */
-  if((linkaddr_node_addr.u8[0] == COORD00 &&
-     linkaddr_node_addr.u8[1] == COORD01) || 
-     (linkaddr_node_addr.u8[0] == COORD10 &&
-     linkaddr_node_addr.u8[1] == COORD11)) {
-    is_coordinator = 1;
-    leds_on(LEDS_RED);
+  int j;
+  for(j = 0 ; j < NUMRECORDS; j++){
+    if((linkaddr_node_addr.u8[0] == coords[j].first) &&
+     (linkaddr_node_addr.u8[1] == coords[j].second)) {
+        is_coordinator = 1;
+        leds_on(LEDS_RED);
+    }
   }
     
   if(linkaddr_node_addr.u8[0] == DRONE0 &&
@@ -391,11 +412,18 @@ PROCESS_THREAD(main_process, ev, data)
     // unicast_open(&uc_drone, 137, &drone_callbacks);
     unicast_open(&uc_drone_1, 143, &drone_callbacks_1);
     NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL,11);
+    /* Create records in drone */
+    for(j = 0; j < NUMRECORDS;j++){
+        (records[j].from).u8[0] = coords[j].first;
+        (records[j].from).u8[1] = coords[j].second;
+    }
   }else if(is_coordinator){
     broadcast_open(&uc_drone, 137, &drone_callbacks);
     // unicast_open(&uc_drone, 137, &drone_callbacks);
     unicast_open(&uc_drone_1, 143, &drone_callbacks_1);
-    unicast_open(&uc, 146, &sensor_callbacks);  
+    unicast_open(&uc, 146, &sensor_callbacks);
+    (records[0].to).u8[0] = DRONE0;
+    (records[0].to).u8[1] = DRONE1;
   }else if(is_sensor){
     unicast_open(&uc, 146, &sensor_callbacks);  
   }
@@ -436,8 +464,8 @@ PROCESS_THREAD(main_process, ev, data)
       }else if(is_sensor){
         printf("Sending Sensor\n");
         packetbuf_copyfrom("SENSOR", 6);
-        addr.u8[0] = COORD00;
-        addr.u8[1] = COORD01;
+        addr.u8[0] = coords[0].first;
+        addr.u8[1] = coords[0].second;
         sending = 1;
         unicast_send(&uc, &addr);
       }
