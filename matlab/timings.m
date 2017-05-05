d3242931a62edd5e50ce1cd3b387db1ec15d999d
lakshbhatia <bhatialaksh3@gmail.com>
2017-05-05 15:58:39 +0100

    Changing tx range

--- matlab/timings.m
+++ matlab/timings.m
@@ -1,14 +1,16 @@
 % Modelling of broadcast duration, speed of drone, range of
 % communication, number of packets, time for sending stuff
-hit_location = 70;
-desired_speed = 5;
-disp(strcat('Possible or not with changing distance:',num2str(changing_distance(hit_location))));
-disp(strcat('Possible or not with changing speed:',num2str(changing_speed(hit_location,desired_speed))));
+hit_location = 50;          %distance
+desired_speed = 10;          %m/s
+broadcast = 10;             %seconds
+range = 100;                %meters
 
-function possible = changing_distance(hit_distance)
-    broadcast = 1;             %seconds
-    speed = 10;                 %m/s
-    range = 10;                %meters
+disp(strcat('Possible or not with changing hit distance:',num2str(changing_distance(hit_location,desired_speed,broadcast,range))));
+disp(strcat('Possible or not with changing speed:',num2str(changing_speed(hit_location,desired_speed,broadcast,range))));
+disp(strcat('Possible or not with changing range:',num2str(changing_range(hit_location,desired_speed,broadcast,range))));
+
+%changes hit location i.e when the packet is received
+function possible = changing_distance(hit_distance,speed,broadcast,range)
     num_packets = 15;   
     time_per_packet = 0.06;    %seconds 
     %3.47s for 10seconds timer,2s backoff
@@ -43,11 +45,12 @@ function possible = changing_distance(hit_distance)
         j = j + 1;
         output1(j) = ((range - i)/speed) - total_time;
     end
+    figure;
     plot(output1);
     ax = gca;                        % gets the current axes
     ax.XAxisLocation = 'origin';     % sets them to zero
     ax.YAxisLocation = 'origin';     % sets them to zero
-    xlabel('Range of communication');
+    xlabel('Hit distances');
     ylabel('Free time/Possible backoff times');
     
     if output1(hit_distance) > 0
@@ -58,10 +61,66 @@ function possible = changing_distance(hit_distance)
     backoff = 2;                %seconds
 end
 
-function possible = changing_speed(hit_distance,desired_speed)
-    broadcast = 1;             %seconds
+% changes range of communication
+function possible = changing_range(hit_distance,speed,broadcast,range)
+    if hit_distance > range
+        disp('Hit distance cannot be further from range');
+        possible = false;
+        return;
+    end
+    max_range = 100;
+    num_packets = 15;   
+    time_per_packet = 0.06;    %seconds 
+    %3.47s for 10seconds timer,2s backoff
+    %1s for 15 packets of size 49bytes each
+    total_time_packets = num_packets * time_per_packet;
+    check_packets = 2;          %seconds
+    if(check_packets < total_time_packets)
+        disp('Check packets smaller than total time required to send');
+        possible = false;
+        return;
+    end
+    
+    total_time = check_packets;
+    
+	%dropped packets
+    r = floor((4)*rand(1));
+	if r(1) ~= 0
+        str = strcat('Dropped packets:',num2str(r(1)));
+        disp(str);
+        total_time = total_time + (r(1) + 1) * time_per_packet;
+    end 
+    total_time = total_time + 1 * time_per_packet; %sending backoff packet
+        
+    output1 = zeros(100,1);
+    no_of_broadcasts = zeros(100,1);
+    j = 0;
+    for i = linspace(1,max_range,100)
+        j = j + 1;
+        output1(j) = ((i - hit_distance)/speed) - total_time;
+        no_of_broadcasts(j) = i / (broadcast * speed);
+    end
+    
+    figure;
+    plot(linspace(1,max_range,100),output1);
+    ax = gca;                        % gets the current axes
+    ax.XAxisLocation = 'origin';     % sets them to zero
+    ax.YAxisLocation = 'origin';     % sets them to zero
+    xlabel('Range(TX)');
+    ylabel('Free time/Possible backoff times');
+    
+%     disp(no_of_broadcasts);
+    if output1(range) > 0
+        possible = true;
+    else
+        possible = false;
+    end
+    backoff = 2;                %seconds
+end
+
+% changes speed of drone
+function possible = changing_speed(hit_distance,desired_speed,broadcast,range)
     max_speed = 10;                 %m/s
-    range = 100;                %meters
     num_packets = 15;   
     time_per_packet = 0.06;    %seconds 
     %3.47s for 10seconds timer,2s backoff
@@ -94,6 +153,7 @@ function possible = changing_speed(hit_distance,desired_speed)
         no_of_broadcasts(j) = range / (broadcast * i);
     end
     
+    figure;
     plot(linspace(1,max_speed,100),output1);
     ax = gca;                        % gets the current axes
     ax.XAxisLocation = 'origin';     % sets them to zero
@@ -101,7 +161,7 @@ function possible = changing_speed(hit_distance,desired_speed)
     xlabel('Speed');
     ylabel('Free time/Possible backoff times');
     
-    disp(no_of_broadcasts);
+%     disp(no_of_broadcasts);
     if output1(desired_speed * 10) > 0
         possible = true;
     else
